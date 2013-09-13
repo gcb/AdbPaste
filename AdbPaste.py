41d1e93db7799bdc33acca2f085d43dd7c2a9376
gcb <descartavel1@gmail.com>
2013-09-13 14:27:18 -0700

    license moved to apache2

--- AdbPaste.py
+++ AdbPaste.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 
-# GPV v3 code.
-# writting in a hurry. No unit tests because most of the issues i get is because of the adb->sh translation
-# if I could mock that up I wouldn't need this file...
+#
+#   Copyright 2013 https://github.com/gcb
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
 import sys,os,itertools
 class AdbPaste:
