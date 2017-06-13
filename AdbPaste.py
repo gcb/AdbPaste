c8699adaea5c3ffff4b7febf9688ad7981609746
koroshiya <mitchell@hubsystems.com.au>
2017-06-13 14:30:27 +1000

    Addresses issue 6

--- AdbPaste.py
+++ AdbPaste.py
@@ -20,75 +20,6 @@ import sys,os,itertools
 class AdbPaste:
 	"Pass a long string as input to an android device/emulator"
 
-	key_dict = {
-		#key is the string, value is the keycode the emulator expects to generate that string
-		"0":7,
-		"1":8,
-		"2":9,
-		"3":10,
-		"4":11,
-		"5":12,
-		"6":13,
-		"7":14,
-		"8":15,
-		"9":16,
-		"*":17,
-		"#":18,
-		"A":29,
-		"B":30,
-		"C":31,
-		"D":32,
-		"E":33,
-		"F":34,
-		"G":35,
-		"H":36,
-		"I":37,
-		"J":38,
-		"K":39,
-		"L":40,
-		"M":41,
-		"N":42,
-		"O":43,
-		"P":44,
-		"Q":45,
-		"R":46,
-		"S":47,
-		"T":48,
-		"U":49,
-		"V":50,
-		"W":51,
-		"X":52,
-		"Y":53,
-		"Z":54,
-		",":55,
-		".":56,
-		"	":61,
-		" ":62,
-		"\n":66,
-		"`":68,
-		"-":69,
-		"=":70,
-		"[":71,
-		"]":72,
-		"\\":73,
-		";":74,
-		"'":75,
-		"/":76,
-		"@":77,
-		"+":81,
-		"(":162,
-		")":163,
-		#// note how there are no :'"? and others... sigh. can't standardize one solution for it all
-	}
-
-	#// charaters that must be sent as keyevent because as string sh will complain.
-	#// there is nothing i can do when calling it on windows because adb will just
-	#// pass it forward to sh and things break.
-	trouble = [' ', '\n', '	'] # i think space is only needed in adb.exe->sh... when running directly in unix it may not be needed
-	if sys.platform != "win32":
-		trouble.append('`')
-	inconvenience = [';', ')' ,'(', "'", '\\', '&', '#', '<', '>', '|']
-
 	def __init__(self, input_string=""):
 		self.addString( input_string )
 	
@@ -96,86 +27,27 @@ class AdbPaste:
 		self.string_data = input_string
 
 	def getKeys(self, fast=False):
-		"thanks to some keys not being available, e.g. colon, we return an array of keycodes (int) or strings."
-		r = []
-		count = 0
-		for c in self.string_data:
-			count += 1
-			# if char is in trouble list, create a new int element in the output
-			if c in self.trouble:
-				t = self.translate(c)
-				r.append( t )
-
-			# work around a bug in the emulator... if the browser starts to look on google
-			#  while this script is 'typing' in the address bar, anything longer than 10 or so
-			#  chars will fail on my box... so just make it slow here too... man, i hate the emulator.
-			#elif len(r) < 10: # or len(r[-1]) > 10:
-			else:
-				#// if the last element is a safe string, continue to add to it
-				# before anything, escape if needed
-				if c == '"': # added this to CMD.exe issues, TODO: test on other platforms
-					c = '\\\\\\"' # this will become \\\" to CMD when passing to adb.exe, which will become \" to sh, and finally " to the device
-				# special case for > ,only way to escape them in windows when it is in a double quote and followed by anything is with ^
-				# but adding the ^ in front, if there's no double quote in the string, CMD will not treat ^ as a special char and send it along
-				elif sys.platform == "win32" and c == ">":
-					print 'greater than'
-					if len(r)>0 and isinstance(r[-1], str) and '"' in r[-1]:
-						c = "\\^>"
-					else:
-						c = "\\>"
-				# ^ is a escape char in windows. it will be ignored
-				elif sys.platform == "win32" and c == "^":
-					c = "^^"
-				elif c in self.inconvenience:
-					c = '\\' + c
-
-				#// here is something weird... $ does not need to be encoded (\$ results in \$ typed in the emulator) but it will
-				#// also fail if it's not the last char in the string. proably sh at some point try to do variable subst
-				if len(r) > 0 and isinstance(r[-1], str) and len(r[-1]) > 0 and r[-1][-1] != '$':
-					# work around a bug in the emulator... if the browser starts to look on google
-					#  while this script is 'typing' in the address bar, anything longer than 10 or so
-					#  chars will fail on my box... so just make it slow here too... man, i hate the emulator.
-					if not fast and count > 7 and isinstance(r[-1], str) and len(r[-1]) > 7:
-						r.append( c )
-					else:
-						r[-1] += c
-
-				else:
-					#// otherwise, start a new safe string batch
-					r.append( c )
-
-		# ? is a special case at least on win32. "asd?" and "?a" ok. "?" fail. "\?" shows "d".
-		# so we can't escape it, but we can make it work by appending something when it is alone.
-		# but then we have to delete that something... argh
-		# also we only need to pad it if the string isn't already paddig it. but in the main loop we aren't looking forward.
-		# so the solution here is to look at instances where it happens without any padding, and change to ?a<backspace>... sigh
-		r = itertools.chain.from_iterable(("?a", 67) if item == "?" else (item, ) for item in r)
+		if len(self.string_data) > 0:
+			r = "$'" + ''.join(['\\x' + c.encode('hex') for c in self.string_data]) + "'"
 
 		return r
 
-	def sendKeys(self, key_list, device=False, dryrun=False):
-		for k in key_list:
-			self.send( k, device, dryrun );
+	def sendKeys(self, string, device=False, dryrun=False):
+		self.send( string, device, dryrun );
 
-	def send( self, key, device=False, dryrun=False ):
+	def send( self, string, device=False, dryrun=False ):
 		"sends a single key to the device/emulator"
-		print('sending', key)
+		print('sending', string)
 		if dryrun: return
 		cmd = 'adb'
 		if isinstance(device, str):
 			cmd += ' -s ' + device
-		if( isinstance(key, int) ):
-			ret = os.system(cmd + ' shell input keyevent %d'%key)
-		else:
-			ret = os.system(cmd + ' shell input text "' + key + '"')
+		ret = os.system(cmd + ' shell input text "' + string + '"')
 		if ret != 0:
 			if isinstance(ret, int):
 				sys.exit( ret )
 			else:
 				sys.exit( 1 )
-
-	def translate( self, char ):
-		return self.key_dict[char] #// will fail on unkown values, so we can add them :)
 
 def displayHelp():
 	print """
